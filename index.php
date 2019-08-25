5927d6132b7a78e45b47415159b17ffac1693dcc
sayyadiram <irmsyd94@gmail.com>
2019-08-25 22:48:55 +0530

    react native registration using  web view

--- index.php
+++ index.php
@@ -1,111 +1,192 @@
 <?php 
     //inlcude file
-    include_once('connection.php');
+    include_once('registration_details.php');
 ?>
-
 
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Registration_Form</title>
-	<link rel="stylesheet" type="text/css" href="./style.css">
-
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-   
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
-    <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" type="text/css" href="./style.css">
 </head>
 <body>
-	<?php if(isset($_SESSION['msg'])):?>
-        <div class="msg">
-            <?php
-                echo $_SESSION['msg'];
-                unset($_SESSION['msg']);
-            ?>
-        </div>
-    <?php endif ?>
-	<form  method="post" action="connection.php" style="padding-left: 30px;">
-		<h1> Registraion Form </h1>
-
-		<input type="text" name="user_name" placeholder="User Name" required><br><br>
-		
-		<input type="email" name="email_id" placeholder="Email" required><br><br>
-		
-		<input type="Password" name="password" placeholder="Password" required>
-		
-		<input type="Password" name="cnfrm_pass" placeholder="Password" required>
-		
-		<br><br>
-		
-		<h1>Personal Details</h1>
-		
-		<br><br>
-
-		<input type="text" name="first_name" placeholder="First Name" required>
-		
-		<input type="text" name="last_name" placeholder="Last Name"required><br><br>
-	       	
-	   	<input id="datepicker" width="276" /  name="dob" placeholder="Birth_Date" required>
-   			 <script>
-        		$('#datepicker').datepicker({ });
-    		</script>
-	   
-	    <br><br>
-
-	    <label>Gender </lable>
-	   	<div style="display: inline;">
-	   		<input type="radio" name="gender" value="male">male
-	   		<input type="radio" name="gender" value="female">female
-	   		<input type="radio" name="gender" value="other">other
-	   	</div><br><br><br>
-	   	    
-	    <textarea name="address" placeholder="Address" required></textarea>
-
-	    <label>Country</label>
-	    <select  name="country" required>
-			<option  value="India">India</option>
-			<option  value="France">France</option>
-			<option  value="Other">Other</option>
-		</select>	     <br><br>
-
-	   	
-	   	
-
-		<label>Status</	<label>
-		<div style="display: inline;">
-			<input type="radio" name="status" value="married">married
-			<input type="radio" name="status" value="unmarried">unmarried
-			<input type="radio" name="status" value="Divorced">Divorced 
-		</div><br><br><br>
-
-		<input type="number" name="ph_no" size="10" placeholder="Phone_Number" required><br><br><br>
-
-		<label>Skill</label>
-		 <div style="display: inline;">
-			<input type="checkbox" name="skill" value="HTML">HTML
-			<input type="checkbox" name="skill" value="CSS">CSS
-			<input type="checkbox" name="skill" value="PHP">PHP 
-			<input type="checkbox" name="skill" value="JS">JS
-			<input type="checkbox" name="skill" value="MySql">MySql
-			<input type="checkbox" name="skill" value="Bootstrap">Bootstrap<br><br>
-		 </div>
-		
-		<input type="text" name="one_word" placeholder="Describe yourself in ONE word" required>
-		
-		<textarea name="hobby" placeholder="Hobby"></textarea><br><br>
-
-		<textarea  name="describe_self" placeholder="Tell About Your Self"></textarea>
-
-		<textarea  name="thinking" placeholder="What you think you can do as a programmer" required></textarea>
-		<br><br>
-		
-		<input type="checkbox" value="" required>terms and condition<br><br>
-		
-		<button  type="submit" name="save" >Save</button>
-	</form>
-
+	<div class="container">
+		<?php if(isset($_SESSION['msg'])):?>
+			<div class="msg">
+				<?php
+					echo $_SESSION['msg'];
+					unset($_SESSION['msg']);
+				?>
+			</div>
+		<?php endif ?>
+		<form  method="post" action="registration_details.php">
+			<div class="row">
+				<h1> Registraion Form </h1>
+			</div>
+			<div class="row">
+      			<div class="col-25">
+        			<label for="usr_name">User Name</label>
+      			</div>
+      			<div class="col-75">
+				  <input type="text" name="user_name" placeholder="User Name" required>
+      			</div>
+    		</div><!--usr_name-->
+			<div class="row">
+      			<div class="col-25">
+        			<label for="usr_mail">Email</label>
+      			</div>
+      			<div class="col-75">
+				  <input type="email" name="email_id" placeholder="Email" required>
+      			</div>
+    		</div><!--user_mail-->
+			<div class="row">
+      			<div class="col-25">
+        			<label for="usr_pass">Password</label>
+      			</div>
+      			<div class="col-75">
+				  <input type="Password" name="password" placeholder="Password" required>
+      			</div>
+    		</div><!--password-->
+			<div class="row">
+      			<div class="col-25">
+        			<label for="usr_cnf_pass">Confirm Password</label>
+      			</div>
+      			<div class="col-75">
+				  <input type="Password" name="cnfrm_pass" placeholder="Password" required>
+      			</div>
+    		</div><!--confirm password-->
+			<div class="row">
+				<h1>Personal Details</h1>
+			</div>
+			<div class="row">
+      			<div class="col-25">
+        			<label for="usr_first_name">First Name</label>
+      			</div>
+      			<div class="col-75">
+				  <input type="text" name="first_name" placeholder="First Name" required>
+      			</div>
+			</div><!--first name-->
+			<div class="row">
+      			<div class="col-25">
+        			<label for="usr_last_name">Last Name</label>
+      			</div>
+      			<div class="col-75">
+				  <input type="text" name="last_name" placeholder="Last Name"required>
+      			</div>
+			</div><!--last name-->
+			<div class="row">
+      			<div class="col-25">
+        			<label for="user_dob">Date of Birth</label>
+      			</div>
+      			<div class="col-75">
+				  <input type="date" name=dob required>
+	        	</div>
+			</div><!--date of birth-->
+			<div class="row">
+      			<div class="col-25">
+        			<label for="usr_gender">Gender</label>
+      			</div>
+      			<div class="col-75">
+					<input type="radio" name="gender" value="male">male
+	   				<input type="radio" name="gender" value="female">female
+	   				<input type="radio" name="gender" value="other">other
+      			</div>
+			</div><!--gender-->
+			<div class="row">
+      			<div class="col-25">
+        			<label for="usr_address">Address</label>
+      			</div>
+      			<div class="col-75">
+				  <textarea name="address" placeholder="Address" required></textarea>
+      			</div>
+			</div><!--address-->
+			<div class="row">
+      			<div class="col-25">
+        			<label for="usr_country">Country</label>
+      			</div>
+      			<div class="col-75">
+					<select  name="country" required>
+						<option  value="India">India</option>
+						<option  value="France">France</option>
+						<option  value="Other">Other</option>
+					</select>
+      			</div>
+			</div><!--country-->
+			<div class="row">
+      			<div class="col-25">
+        			<label for="usr_status">Status</label>
+      			</div>
+      			<div class="col-75">
+					<input type="radio" name="status" value="married">married
+					<input type="radio" name="status" value="unmarried">unmarried
+					<input type="radio" name="status" value="Divorced">Divorced 
+      			</div>
+			</div><!--status-->
+			<div class="row">
+      			<div class="col-25">
+        			<label for="usr_phone_no">Phone Number</label>
+      			</div>
+      			<div class="col-75">
+				  <input type="number" name="ph_no" size="10" placeholder="Phone_Number" required>
+      			</div>
+			</div><!--phone number-->
+			<div class="row">
+      			<div class="col-25">
+        			<label for="usr_skill">Skill</label>
+      			</div>
+      			<div class="col-75">
+					<input type="checkbox" name="skill[]" value="HTML">HTML
+					<input type="checkbox" name="skill[]" value="CSS">CSS
+					<input type="checkbox" name="skill[]" value="PHP">PHP 
+					<input type="checkbox" name="skill[]" value="JS">JS
+					<input type="checkbox" name="skill[]" value="MySql">React js
+					<input type="checkbox" name="skill[]" value="Bootstrap">React Native
+					<input type="checkbox" name="skill[]" value="Bootstrap">MySql
+					<input type="checkbox" name="skill[]" value="Bootstrap">Bootstrap
+      			</div>
+			</div><!--skill-->
+			<div class="row">
+      			<div class="col-25">
+        			<label for="one_word">One word</label>
+      			</div>
+      			<div class="col-75">
+				  <input type="text" name="one_word" placeholder="Describe yourself in ONE word" required>
+      			</div>
+			</div><!--one word-->
+			<div class="row">
+      			<div class="col-25">
+        			<label for="usr_hobby">Hobby</label>
+      			</div>
+      			<div class="col-75">
+					<textarea name="hobby" placeholder="Hobby"></textarea>
+      			</div>
+			</div><!--hobby-->
+			<div class="row">
+      			<div class="col-25">
+        			<label for="abut_u">About you</label>
+      			</div>
+      			<div class="col-75">
+					<textarea  name="describe_self" placeholder="Tell About Your Self"></textarea>
+      			</div>
+			</div><!--describe_self-->
+			<div class="row">
+      			<div class="col-25">
+        			<label for="usr_think">What?</label>
+      			</div>
+      			<div class="col-75">
+				  <textarea  name="thinking" placeholder="What you think you can do as a programmer" required></textarea>
+      			</div>
+			</div><!--thinking-->
+			<div class="row">
+				<input type="checkbox" value="" required>
+				<label>terms and condition</label><br><br>
+			</div><!--confirmation-->
+			<div class="row">
+				<input type="submit" value="Submit" name="save"> 
+			</div><!--submit-->
+		</form>
+	</div><!--close container-->
 </body>
 </html>
